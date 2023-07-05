83883f965e78e4be2304cd92be929443a68f0c7d
Hubert Warchoł <n3tr00x99@gmail.com>
2023-07-05 02:49:12 +0200

    added functions rendering race and qualification results

--- src/components/results/ResultContent.js
+++ src/components/results/ResultContent.js
@@ -1,17 +1,14 @@
-import { fetchRaceResult } from '../../utils/api';
+import { fetchQualifyingResult, fetchRaceResult } from '../../utils/api';
 import createHTMLElement from '../../utils/createHTMLElement';
 import Table from './Table';
+import TableQ from './TableQ';
 
 const TabContent = () => {
-	const qualifyingResultContent = createHTMLElement('div', null, {
-		className: 'results-content__qualifying',
-		attrs: {
-			['data-tab']: 'qualifying',
-		},
-		children: [],
-	});
-
 	const raceResultContent = renderRaceResultContent('current', 'last');
+	const qualifyingResultContent = renderQualifyingResultContent(
+		'current',
+		'last'
+	);
 
 	return createHTMLElement('div', null, {
 		className: 'results-content',
@@ -20,23 +17,65 @@ const TabContent = () => {
 };
 
 export const renderRaceResultContent = (season, round) => {
-	const raceContent = document.querySelector('.results-content__race');
+	const contentContainer = document.querySelector('.results-content__race');
 
-	if (raceContent) raceContent.remove();
+	if (contentContainer) contentContainer.remove();
 
-	const raceResultContent = createHTMLElement('div', null, {
+	const content = createHTMLElement('div', null, {
 		className: ['results-content__race', 'results-content--active'],
 		attrs: {
 			['data-tab']: 'race',
 		},
 	});
 
-	fetchRaceResult(season, round).then(() => {
-		const table = Table();
-		raceResultContent.appendChild(table);
+	const getData = async (season, round) => {
+		try {
+			const response = await fetchRaceResult(season, round);
+			if (response?.error) throw response.message;
+
+			const table = Table();
+			content.appendChild(table);
+		} catch (error) {
+			const text = createHTMLElement('p', error);
+			content.appendChild(text);
+		}
+	};
+
+	getData(season, round);
+
+	return content;
+};
+
+export const renderQualifyingResultContent = (season, round) => {
+	const contentContainer = document.querySelector(
+		'.results-content__qualifying'
+	);
+
+	if (contentContainer) contentContainer.remove();
+
+	const content = createHTMLElement('div', null, {
+		className: 'results-content__qualifying',
+		attrs: {
+			['data-tab']: 'qualifying',
+		},
 	});
 
-	return raceResultContent;
+	const getData = async (season, round) => {
+		try {
+			const response = await fetchQualifyingResult(season, round);
+			if (response?.error) throw response.message;
+
+			const table = TableQ();
+			content.appendChild(table);
+		} catch (error) {
+			const text = createHTMLElement('p', error);
+			content.appendChild(text);
+		}
+	};
+
+	getData(season, round);
+
+	return content;
 };
 
 export default TabContent;
