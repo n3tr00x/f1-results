78ebd9bbf53237543ac260ae7889a94401005ecf
Hubert Warchoł <n3tr00x99@gmail.com>
2023-07-10 01:37:16 +0200

    removed modal components from root and set these as siblings of root

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import App from './javascript/App';
+import SeasonModal from './javascript/components/SeasonModal';
+import SeasonScheduleModal from './javascript/components/SeasonScheduleModal';
 import './styles/main.scss';
 
 const app = App();
+const seasonModal = SeasonModal();
+const seasonScheduleModal = SeasonScheduleModal();
 
 document.querySelector('body').appendChild(app);
+document.querySelector('body').appendChild(seasonModal);
+document.querySelector('body').appendChild(seasonScheduleModal);
--- src/javascript/App.js
+++ src/javascript/App.js
@@ -1,24 +1,18 @@
-import { getSeason, setCircuits } from './state/state';
-import { fetchAllCircuits } from './utils/api';
 import createHTMLElement from './utils/createHTMLElement';
-import CircuitsModal from './components/SeasonScheduleModal';
 import Header from './components/Header';
 import Navbar from './components/navbar/Navbar';
-import SeasonModal from './components/SeasonModal';
 import Main from './components/Main';
 
 const App = () => {
 	const header = Header();
 	const navbar = Navbar();
 	const main = Main();
-	const seasonModal = SeasonModal();
-	const circuitsModal = CircuitsModal();
 
 	return createHTMLElement('div', null, {
 		attrs: {
 			id: 'root',
 		},
-		children: [header, navbar, main, seasonModal, circuitsModal],
+		children: [header, navbar, main],
 	});
 };
 
