b7d476cd769a295d59525276699ebb0594a42fb3
Hubert Warchoł <n3tr00x99@gmail.com>
2023-07-24 20:20:44 +0200

    add a alt attribute to nav icons

--- src/javascript/components/navbar/ButtonWithIcon.js
+++ src/javascript/components/navbar/ButtonWithIcon.js
@@ -3,7 +3,7 @@ import createHTMLElement from '../../utils/createHTMLElement';
 const ButtonWithIcon = (text, image, buttonName) => {
 	const icon = createHTMLElement('img', null, {
 		classList: 'icon',
-		attrs: { src: image },
+		attrs: { src: image, alt: '' },
 	});
 	const desc = createHTMLElement('span', text);
 	const button = createHTMLElement('button', null, {
